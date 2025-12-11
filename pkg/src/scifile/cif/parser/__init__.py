172630f6b00c7fe78d523c3d825fd20ad93e8693
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-11 23:32:24 +0100

    Update __init__.py

--- pkg/src/scifile/cif/parser/__init__.py
+++ pkg/src/scifile/cif/parser/__init__.py
@@ -1,5 +1,32 @@
-from ._parser_todict import CIFToDictParser
+"""CIF file parser."""
+
+from scifile.typing import FileLike
+from scifile._util import filelike_to_str
+from ._parser_todict import CIFToDictParser, CIFFlatDict
+from ._exception import CIFParsingError
+
+__all__ = [
+    "CIFFlatDict",
+    "CIFParsingError",
+    "parse",
+]
 
 
-def parse(cif_file: str) -> dict:
-    return CIFToDictParser().parse(cif_file)
+def parse(file: FileLike, encoding: str = "utf-8") -> tuple[CIFFlatDict, list[CIFParsingError]]:
+    """Parse a CIF file into a flat dictionary representation.
+
+    Parameters
+    ----------
+    file
+        CIF file to be parsed.
+    encoding
+        Encoding used to decode the file if it is provided as bytes or Path.
+
+    Returns
+    -------
+    tuple[CIFFlatDict, list[CIFParsingError]]
+        A tuple containing the parsed CIF file as a flat dictionary
+        and a list of parsing errors encountered during parsing.
+    """
+    file_content: str = filelike_to_str(file, encoding=encoding)
+    return CIFToDictParser().parse(file_content)
