7add3368e67bd23cc5c6ae74bda5877d1bb75ef2
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-11 20:21:25 +0100

    Create __init__.py

---
+++ pkg/src/scifile/cif/__init__.py
@@ -0,0 +1,14 @@
+"""Crystallographic Information File (CIF).
+
+References
+----------
+- [Official CIF specification](https://www.iucr.org/resources/cif/spec)
+
+Other Python packages with mmCIF support:
+- [mmCIF Core Access Library (by RCSB)](https://github.com/rcsb/py-mmcif)
+- [BioPython](https://github.com/biopython/biopython/blob/master/Bio/PDB/MMCIFParser.py)
+- [BioPandas](https://github.com/BioPandas/biopandas/tree/main/biopandas/mmcif)
+- [Biotite](https://github.com/biotite-dev/biotite/tree/master/src/biotite/structure/io/pdbx)
+"""
+
+from . import read
