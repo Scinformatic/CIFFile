1bcdee083de9310402fb3079dc06d7668deb7c88
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-12 22:36:20 +0100

    Update __init__.py

---
+++ pkg/src/scifile/__init__.py
@@ -0,0 +1,12 @@
+"""SciFile: read and write scientific data files."""
+
+from . import cif # gpf, map, mrc, pdb, pdbqt
+
+__all__ = [
+    "cif",
+    # "gpf",
+    # "map",
+    # "mrc",
+    # "pdb",
+    # "pdbqt",
+]
