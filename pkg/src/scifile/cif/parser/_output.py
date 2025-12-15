44c560127941e48d180f7b5121f5439ecc081906
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-15 07:09:17 +0100

    Update _output.py

--- pkg/src/scifile/cif/parser/_output.py
+++ pkg/src/scifile/cif/parser/_output.py
@@ -2,6 +2,7 @@
 
 
 from typing import TypedDict
+from scifile.cif.typing import BlockCode, FrameCode, DataCategory, DataKeyword, DataValues
 
 
 class CIFFlatDict(TypedDict):
@@ -12,35 +13,20 @@ class CIFFlatDict(TypedDict):
 
     Attributes
     ----------
-    block_code
+    block
         List of block codes (i.e., data block names) for each data item.
-        This is the top-level grouping in a CIF file,
-        where each data item belongs to a specific data block.
-    frame_code
+    frame
         List of frame codes (i.e., save frame names) for each data item.
-        This is the second-level grouping in a CIF file,
-        where data items are either directly under a data block
-        or belong to a specific save frame within a data block.
-        For data items not in a save frame, this value is `None`.
-    loop_code
-        List of loop codes for each data item.
-        A loop code of `0` indicates that the data item is a single item
-        (i.e., not part of a looped table).
-        A positive loop code (1, 2, ...) indicates that the data item
-        is part of a looped table, with the same loop code
-        shared among all data items in that table.
-    data_name
-        List of data names (i.e., data item names) for each data item.
-    data_values
+    category
+        List of data categories for each data item.
+    keyword
+        List of data keywords for each data item.
+    values
         List of data values for each data item.
-        Each data value is represented as a list of strings.
-        For single data items, this list will contain a single string.
-        For tabular (looped) data items, this list will contain multiple strings,
-        corresponding to row values for that data item column in the table.
     """
 
-    block_code: list[str]
-    frame_code: list[str | None]
-    loop_code: list[int]
-    data_name: list[str]
-    data_values: list[list[str]]
+    block: list[BlockCode]
+    frame: list[FrameCode]
+    category: list[DataCategory]
+    keyword: list[DataKeyword]
+    values: list[DataValues]
