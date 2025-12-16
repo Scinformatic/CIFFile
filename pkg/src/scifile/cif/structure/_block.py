d105c97a8e57f7c43035fb686f908be22cad6fbc
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-16 07:06:10 +0100

    Update _block.py

--- pkg/src/scifile/cif/structure/_block.py
+++ pkg/src/scifile/cif/structure/_block.py
@@ -1,16 +1,18 @@
+"""CIF block data structure."""
+
 from typing import Literal
 
 import polars as pl
 
-from ._util import validate_content_df
+from ._base import CIFFileSkeleton
 
 
-class CIFBlock:
+class CIFBlock(CIFFileSkeleton):
     """CIF file data block."""
 
     def __init__(
         self,
-        name: str,
+        code: str,
         content: pl.DataFrame,
         *,
         variant: Literal["cif1", "mmcif"],
@@ -20,53 +22,23 @@ class CIFBlock:
         col_name_key: str,
         col_name_values: str,
     ):
-        df = validate_content_df(
-            content,
+        super().__init__(
+            content=content,
+            variant=variant,
+            validate=validate,
             require_block=False,
+            require_frame=False,
             col_name_block=None,
             col_name_frame=col_name_frame,
             col_name_cat=col_name_cat,
             col_name_key=col_name_key,
             col_name_values=col_name_values,
-        ) if validate else pl.DataFrame(content)
-        if col_name_frame in df.columns:
-            if df.select(pl.col(col_name_frame).is_null().all()).item():
-                df = df.drop(col_name_frame)
-                col_name_frame = None
-                blocktype = "data"
-            else:
-                blocktype = "dict"
-        else:
-            col_name_frame = None
-            blocktype = "data"
+        )
 
-        self._name = name
-        self._df = df
-        self._variant: Literal["cif1", "mmcif"] = variant
-        self._col_frame = col_name_frame
-        self._col_cat = col_name_cat
-        self._col_key = col_name_key
-        self._col_values = col_name_values
-        self._blocktype = blocktype
+        self._code = code
         return
 
     @property
-    def name(self) -> str:
-        """Data block name."""
-        return self._name
-
-    @property
-    def type(self) -> Literal["data", "dict"]:
-        """Type of CIF block
-
-        Either:
-        - "data": Data block contains no save frames; all data items are directly under the block.
-        - "dict": Data block contains save frames; some data items are under save frames.
-        """
-        return self._blocktype
-
-    @property
-    def df(self) -> pl.DataFrame:
-        """Data block content as DataFrame."""
-        return self._df
-
+    def code(self) -> str:
+        """Data block code."""
+        return self._code
