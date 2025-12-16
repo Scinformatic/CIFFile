a279f9ddd70fb6aa956aabca6bf7261bc150088d
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-16 10:04:08 +0100

    Update __init__.py

--- pkg/src/scifile/cif/structure/__init__.py
+++ pkg/src/scifile/cif/structure/__init__.py
@@ -1,5 +1,40 @@
+from typing import Literal
+
+from ._block import CIFBlock
+from ._category import CIFDataCategory
 from ._file import CIFFile
+from ._frame import CIFFrame
+
+
+from scifile.typing import DataFrameLike
+
 
 __all__ = [
+    "CIFBlock",
+    "CIFDataCategory",
     "CIFFile",
+    "CIFFrame",
 ]
+
+
+def file(
+    content: DataFrameLike,
+    *,
+    variant: Literal["cif1", "mmcif"] = "mmcif",
+    validate: bool = True,
+    col_name_block: str = "block",
+    col_name_frame: str = "frame",
+    col_name_cat: str = "category",
+    col_name_key: str = "keyword",
+    col_name_values: str = "values",
+) -> CIFFile:
+    return CIFFile(
+        content=content,
+        variant=variant,
+        validate=validate,
+        col_name_block=col_name_block,
+        col_name_frame=col_name_frame,
+        col_name_cat=col_name_cat,
+        col_name_key=col_name_key,
+        col_name_values=col_name_values,
+    )
