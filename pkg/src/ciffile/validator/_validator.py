1d5e8a937bb5cbb66ecd8c883e0ed29703751587
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-12-19 09:39:51 +0100

    Update _validator.py

--- pkg/src/ciffile/validator/_validator.py
+++ pkg/src/ciffile/validator/_validator.py
@@ -1,5 +1,15 @@
 """CIF file validator."""
 
+from typing import Any
+
 
 class CIFFileValidator:
-    pass
+
+    def __init__(self, dictionary: dict) -> None:
+        self._dict = dictionary
+        return
+
+    @property
+    def dict(self) -> dict[str, Any]:
+        """Dictionary metadata."""
+        return self._dict
